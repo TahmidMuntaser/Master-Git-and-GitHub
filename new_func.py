b61d5ee32d6c8c2a71f079c8aa985754f9ad9f76
Tahmid <tahmid25muntaser@gmail.com>
2026-06-23 15:52:34 +0600

    test: git rebase

--- new_func.py
+++ new_func.py
@@ -1,8 +1,8 @@
 def sum(num1, num2):
     print(num1 + num2)
 
-def mul():
-    pass
+def mul(num1, num2):
+    print(num1 * num2)
 
 
 def div(num1,num2):
@@ -10,5 +10,5 @@ def div(num1,num2):
 
 
 sum(2,2)
-mul()
+mul(8, 8)
 div(10,5)
